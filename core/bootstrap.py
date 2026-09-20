5469cc13b035cc1a9a27412d6b5359d62a062316
SASESseed <lhs15814886145@gmail.com>
2026-09-20 11:43:15 +0800

    auto: t_1789875793554

--- core/bootstrap.py
+++ core/bootstrap.py
@@ -17,6 +17,7 @@ from .services import pattern_service
 from .services import pattern_service
 from . import backup_service
 from .api_routes import (
+    upload_routes,
     auth_routes,
     seed_routes,
     credit_routes,
@@ -199,6 +200,7 @@ def create_app() -> FastAPI:
     app.include_router(compute_routes.router)
     app.include_router(yunchong_routes.router)
     app.include_router(swarm_routes.router)
+    app.include_router(upload_routes.router)
 
     @app.get("/static/index.html", response_class=HTMLResponse)
     async def serve_index():
